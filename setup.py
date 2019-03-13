f78de2fa83d48de89fe500ca85bdfc24285e3004
Sander in 't Veld <sander@abunchofhacks.coop>
2019-03-13 17:36:21 +0100

    Package metadata.

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ module1 = Extension('caramelrecolor',
 	sources = ['src/caramelrecolor.c'])
 
 setup (name = 'Caramel Recolor',
-	version = '0.1',
+	version = '1.0',
+	description = 'Reimplementation of the Gearhead Caramel recolor algorithm',
+	author = 'Sander in \'t Veld',
+	author_email = 'sander@abunchofhacks.coop',
 	ext_modules = [module1])
