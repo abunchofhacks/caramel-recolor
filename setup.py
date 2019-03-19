f777aa8cf940b3a7281ba2e572cab487ef9f6dc6
Sander in 't Veld <sander@abunchofhacks.coop>
2019-03-19 13:26:48 +0100

    Added GNU LGPL v3 license.

--- setup.py
+++ setup.py
@@ -9,10 +9,21 @@
 #
 # Copyright (c) 2019 A Bunch of Hacks
 #
-# License to be determined.
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <https://www.gnu.org/licenses/>.
 #
 # [authors:]
-# Sander in 't Veld
+# Sander in 't Veld (sander@abunchofhacks.coop)
 #
 
 from distutils.core import setup, Extension
